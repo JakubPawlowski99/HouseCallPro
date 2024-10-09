d8056293eca5584b233de0503ac021d84b4a6794
Jakub Pawłowski <kuba.p99@gmail.com>
2024-10-09 23:17:32 +0200

    Added application.html and changed its controller to prepare for future gui

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   def authenticate_user!
     api_key = ApiKey.find_by(token: request.session[:token])
     @current_user = api_key&.bearer
-    head :unauthorized unless @current_user if @current_user.nil?
+    head :unauthorized if @current_user.nil?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root "home#index"
+
   scope '/api' do
     post :assign, to: 'devices#assign'
     post :unassign, to: 'devices#unassign'
